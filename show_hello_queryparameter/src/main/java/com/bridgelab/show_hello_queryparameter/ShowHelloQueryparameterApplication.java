5438ef2da56b3987e49883b4576b74e237cc173a
Mohitsoni887 <mohitsoni9886921@gmail.com>
2025-02-21 13:08:21 +0530

    [Mohit Soni] Add. Show Hello Using Query Parameter

---
+++ show_hello_queryparameter/src/main/java/com/bridgelab/show_hello_queryparameter/ShowHelloQueryparameterApplication.java
@@ -0,0 +1,14 @@
+package com.bridgelab.show_hello_queryparameter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@SpringBootApplication
+public class ShowHelloQueryparameterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShowHelloQueryparameterApplication.class, args);
+	}
+
+}
---
+++ show_hello_queryparameter/src/test/java/com/bridgelab/show_hello_queryparameter/ShowHelloQueryparameterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bridgelab.show_hello_queryparameter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShowHelloQueryparameterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
